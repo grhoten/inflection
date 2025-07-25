8677eec35e50d7bf1645284d1cbaeba0977d9469
grhoten <grhoten@users.noreply.github.com>
2025-07-25 07:36:40 +0000

    Deploying to gh-pages from @ grhoten/inflection@6849342eba017b06618231033436367b6426f206 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['begin_0',['begin',['../classinflection_1_1dictionary_1_1DictionaryKeyIterator.html#a4db5bdfee20f226592b59362d891cdab',1,'inflection::dictionary::DictionaryKeyIterator']]],
   ['belgium_5fdutch_1',['BELGIUM_DUTCH',['../classinflection_1_1util_1_1LocaleUtils.html#a9e5709ba319071f4cbe9d4fb1a3ee269',1,'inflection::util::LocaleUtils']]],
   ['belgium_5ffrench_2',['BELGIUM_FRENCH',['../classinflection_1_1util_1_1LocaleUtils.html#a7821b3a6837b405f466f46a654039f8e',1,'inflection::util::LocaleUtils']]],
-  ['boundedvalue_3',['boundedValue',['../classinflection_1_1dialog_1_1SemanticFeature.html#a1812c25a67e17d105534931a2747223f',1,'inflection::dialog::SemanticFeature']]],
+  ['boundedvalue_3',['boundedValue',['../classinflection_1_1dialog_1_1SemanticFeature.html#a122a63b49e5c680341ae5a0ea55c53e1',1,'inflection::dialog::SemanticFeature']]],
   ['brazil_4',['BRAZIL',['../classinflection_1_1util_1_1LocaleUtils.html#a9d1392ff3568fb628a7a7c750840c593',1,'inflection::util::LocaleUtils']]],
   ['build_20instructions_5',['Build instructions',['../index.html#autotoc_md13',1,'']]],
   ['building_6',['Building',['../index.html#autotoc_md8',1,'']]],
--- search/functions_b.js
+++ search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['languagegrammarfeatures_5ffeature_0',['LanguageGrammarFeatures_Feature',['../classinflection_1_1lang_1_1features_1_1LanguageGrammarFeatures__Feature.html#a419af995907992e8d91e79a84fd117c8',1,'inflection::lang::features::LanguageGrammarFeatures_Feature']]],
+  ['languagegrammarfeatures_5ffeature_0',['LanguageGrammarFeatures_Feature',['../classinflection_1_1lang_1_1features_1_1LanguageGrammarFeatures__Feature.html#a04ec0e22da93ae4f3b295e7894be4034',1,'inflection::lang::features::LanguageGrammarFeatures_Feature']]],
   ['languagegrammarfeatures_5fgrammarfeatures_1',['LanguageGrammarFeatures_GrammarFeatures',['../classinflection_1_1lang_1_1features_1_1LanguageGrammarFeatures__GrammarFeatures.html#a7ec2ded7d869526a2d123c6be4e58a44',1,'inflection::lang::features::LanguageGrammarFeatures_GrammarFeatures']]],
   ['lithuania_2',['LITHUANIA',['../classinflection_1_1util_1_1LocaleUtils.html#a4c77509d935acb7e25414d9626232261',1,'inflection::util::LocaleUtils']]],
   ['lithuanian_3',['LITHUANIAN',['../classinflection_1_1util_1_1LocaleUtils.html#a66606873fd0b919c6d6e7ab44daaea76',1,'inflection::util::LocaleUtils']]],
